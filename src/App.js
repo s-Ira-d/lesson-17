2c6f82a5c1b020d12f3b7c25f435bfba84e941b2
s-Ira-d <20irinka12@gmail.com>
2026-05-19 20:57:43 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import "./App.css";
 import { Counter } from "./components/Counter.jsx";
-import { ChangeColor } from "./components/ChangeColor.jsx";
 import { Clock } from "./components/Clock.jsx";
 import { Pockemon } from "./components/Pockemon.jsx";
 import { Pole } from "./components/Pole.jsx";
-import { Colors } from "./components/Colors.jsx"
+import { Colors } from "./components/Colors.jsx";
 import { ToDoList } from "./components/ToDoList.jsx";
 
 function App() {
   return (
     <div className="App">
-     <ToDoList/>
+      <Counter />
+      <Clock />
+      <Pockemon />
+      <Pole />
+      <Colors />
+      <ToDoList />
     </div>
   );
 }
